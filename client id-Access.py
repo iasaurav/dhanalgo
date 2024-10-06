6315f813a7126a0f403359855f8ddf7ae9b5213a
iasaurav <101176678+iasaurav@users.noreply.github.com>
2024-10-06 12:30:11 +0530

    Update client id-Acess.py

--- client id-Access.py
+++ client id-Access.py
@@ -15,3 +15,11 @@ file_name = "dhan.xlsx"
 df.to_excel(file_name, index=False)
 
 print(f"Data saved to {file_name}")
+
+
+
+from dhanhq import dhanhq
+
+client_id = "1101112645"
+access_token = "b"
+dhan = dhanhq(client_id, access_token)

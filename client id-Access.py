ac4b43e39b9abac1ab07fc86d3b297eebabba92b
iasaurav <101176678+iasaurav@users.noreply.github.com>
2024-10-06 10:48:43 +0530

    Create client id-Access.py

---
+++ client id-Access.py
@@ -0,0 +1,17 @@
+
+import pandas as pd
+
+# Data to save
+data = {
+    "client_id": ["1101112645"],
+    "access_token": ["tu"]
+}
+
+# Create a DataFrame
+df = pd.DataFrame(data)
+
+# Save to Excel file
+file_name = "dhan.xlsx"
+df.to_excel(file_name, index=False)
+
+print(f"Data saved to {file_name}")
